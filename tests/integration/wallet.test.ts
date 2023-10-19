b93e7915de29965c1601857485b2a42cc6e1a116
danijelTxFusion <danijel@txfusion.com>
2023-10-20 01:12:38 +0200

    feat: add `AdapterL2.getDeploymentNonce` method

--- tests/integration/wallet.test.ts
+++ tests/integration/wallet.test.ts
@@ -139,6 +139,13 @@ describe("Wallet", () => {
         });
     });
 
+    describe("#getDeploymentNonce()", () => {
+        it("should return a deployment nonce", async () => {
+            const result = await wallet.getDeploymentNonce();
+            expect(result).not.to.be.null;
+        });
+    });
+
     describe("#populateTransaction()", () => {
         it("should return populated transaction with default values if are omitted", async () => {
             const tx = {
