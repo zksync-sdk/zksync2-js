46c950434584e0d9c49bf025bd7a69dd912dabb4
danijelTxFusion <danijel@txfusion.com>
2023-10-27 02:58:24 +0200

    docs: document `ContractFactory.deploy` method

--- src/contract.ts
+++ src/contract.ts
@@ -118,6 +118,40 @@ export class ContractFactory extends ethers.ContractFactory {
         return tx;
     }
 
+    /**
+     * Deploys a new contract or account instance on the Ethereum blockchain.
+     *
+     * @async
+     * @param {...Array<any>} args - Constructor arguments for the contract followed by optional
+     * {@link ethers.PayableOverrides|overrides}. When deploying with CREATE2 opcode slat must be present in overrides.
+     *
+     *
+     * @example
+     * // Deploy with constructor arguments only using CREATE opcode
+     * const deployedContract = await contractFactory.deploy(arg1, arg2, ...);
+     *
+     * // Deploy with constructor arguments, and factory dependencies using CREATE opcode
+     * const deployedContractWithSaltAndDeps = await contractFactory.deploy(arg1, arg2, ..., {
+     *   customData: {
+     *     factoryDeps: ['0x...']
+     *   }
+     * });
+     *
+     * // Deploy with constructor arguments and custom salt using CREATE2 opcode
+     * const deployedContractWithSalt = await contractFactory.deploy(arg1, arg2, ..., {
+     *   customData: {
+     *     salt: '0x...'
+     *   }
+     * });
+     *
+     * // Deploy with constructor arguments, custom salt, and factory dependencies using CREATE2 opcode
+     * const deployedContractWithSaltAndDeps = await contractFactory.deploy(arg1, arg2, ..., {
+     *   customData: {
+     *     salt: '0x...',
+     *     factoryDeps: ['0x...']
+     *   }
+     * });
+     */
     override async deploy(...args: Array<any>): Promise<Contract> {
         const contract = await super.deploy(...args);
 
